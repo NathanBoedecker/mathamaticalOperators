721e5bb2e5737e76491451ab18ec255243811d59
NathanBoedecker <57597199+NathanBoedecker@users.noreply.github.com>
2019-11-26 19:56:08 -0600

    Initial commit

---
+++ practiceMathOperators.py
@@ -0,0 +1,65 @@
+'''
+Created on Nov 26, 2019
+
+@author: ITAUser
+'''
+from unittest.test.testmock.testmock import something
+
+x = 4 + 4
+
+print(x)
+
+y = x + 1
+
+print(y)
+
+z = x + y
+
+print(z)
+
+x = x + 1
+
+print(x)
+
+string1 = "yee"
+
+string2 = "haw"
+
+string3 = " haw"
+
+stringCombo = string1 + string2
+
+stringSpace = string1 + string3
+
+print(stringCombo)
+print(stringSpace)
+
+x = x - 1
+
+print(x)
+
+x = x - 1
+
+print(x)
+
+x = 2 * 2
+
+print(x)
+
+string = "E" * 500
+print(string)
+
+x = 4 / 4
+print(x)
+
+X = 9 // 4
+print(x)
+
+x = 4 % 3
+print(x)
+
+x = 2 ** 4
+print(x)
+
+
+
